217a9bd14b39fbe8c7cb4adccc71a8b136449c70
hilal <hasmat@sfu.ca>
2018-01-14 04:07:24 -0800

    Show Password checkBox now functional

--- app/src/main/java/com/nwhacks/connieho/sampleapplication/activity/AuthenticationFragment.java
+++ app/src/main/java/com/nwhacks/connieho/sampleapplication/activity/AuthenticationFragment.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -33,6 +36,7 @@ public class AuthenticationFragment extends DialogFragment {
         final View view =  inflater.inflate(R.layout.authentication_layout, null);
         Button connectButton = (Button) view.findViewById(R.id.connect_button);
         Button cancelButton = (Button) view.findViewById(R.id.cancel_button);
+        final CheckBox checkBox = (CheckBox) view.findViewById(R.id.show_password_check_box);
         setCancelable(true);
 
         connectButton.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +49,18 @@ public class AuthenticationFragment extends DialogFragment {
             @Override
             public void onClick(View v) {
                 cancel();
+            }
+        });
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                EditText passwordEditText = ((EditText)view.findViewById(R.id.password_edit_text));
+                if (checkBox.isChecked()){
+                    passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+                else{
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD  | InputType.TYPE_CLASS_TEXT );
+                }
             }
         });
         return view;
