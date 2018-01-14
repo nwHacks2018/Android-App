a02feaa36623eef54cc8b4f375a1bce8236240ef
Jeffrey Leung <jleung513@gmail.com>
2018-01-13 17:43:40 -0800

    Add NetworkRepository to global application level

--- app/src/main/java/com/nwhacks/connieho/sampleapplication/application/GlobalServices.java
+++ app/src/main/java/com/nwhacks/connieho/sampleapplication/application/GlobalServices.java
@@ -1,5 +1,16 @@
 package com.nwhacks.connieho.sampleapplication.application;
 
+import com.nwhacks.connieho.sampleapplication.service.NetworkRepository;
+
 public class GlobalServices {
+
+    private NetworkRepository networkRepository;
+
+    public NetworkRepository getNetworkRepository() {
+        if(networkRepository == null) {
+            networkRepository = new NetworkRepository();
+        }
+        return networkRepository;
+    }
 
 }
